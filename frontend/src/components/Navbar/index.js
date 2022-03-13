14d67d9f9d1d0a95d9ab3ad9c02703ad72306db7
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2022-03-13 21:53:22 +0700

    add to cart

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 const Navbar = ({ click }) => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -15,7 +23,7 @@ const Navbar = ({ click }) => {
             <i className="fas fa-shopping-cart"></i>
             <span>
               Cart
-              <span className="cartlogo__badge">0</span>
+              <span className="cartlogo__badge">{getCartCount()}</span>
             </span>
           </Link>
         </li>
--- frontend/src/screens/CartScreen/index.js
+++ frontend/src/screens/CartScreen/index.js
@@ -1,22 +1,55 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import CartItem from "../../components/CartItem";
+import { addToCart, removeFromCart } from "../../redux/actions/cartActions";
 import "./style.css";
 
 const CartScreen = () => {
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const qtyChangeHandler = (id, qty) => {
+    dispatch(addToCart(id, qty));
+  };
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
+  const getCartSubTotal = () => {
+    return cartItems.reduce((price, item) => item.price * item.qty + price, 0);
+  };
+
   return (
     <div className="cartscreen">
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
-
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {cartItems.length === 0 ? (
+          <div>
+            Your cart is empty <Link to="/">Go Back</Link>{" "}
+          </div>
+        ) : (
+          cartItems.map((item) => (
+            <CartItem
+              item={item}
+              key={item.product}
+              qtyChangeHandler={qtyChangeHandler}
+              removeFromCartHandler={removeFromCartHandler}
+            />
+          ))
+        )}
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
-          <p>Subtotal (0) items</p>
-          <p>$499.99</p>
+          <p>Subtotal ({getCartCount()}) items</p>
+          <p>${getCartSubTotal().toFixed(2)}</p>
         </div>
         <div>
           <button>Proceed To Checkout</button>
